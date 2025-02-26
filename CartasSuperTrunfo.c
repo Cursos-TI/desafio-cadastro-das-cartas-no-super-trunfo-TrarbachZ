9a3aaa1bc2e546143aa88e2aef1356e3814245b8
alex trarbach <67793329+TrarbachZ@users.noreply.github.com>
2025-02-26 00:24:00 +0000

    adicionei os codigos de cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,20 +7,21 @@ int main() {
 
     char estado1[50], estado2[50];
     char cidade1[50], cidade2[50];
-    char cod1[3], cod2[3];
     int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int turismo1, turismo2;
     float dens1, dens2;
     float pibc1, pibc2;
+    float densInvertida1, densInvertida2;
+    float super1, super2;
   
 // dados cidade 1
 
     printf("Vamos cadastrar a primeira carta.\n");
 
     printf("Digite o nome do estado:\n");
-    scanf(" %[^\n]", estado1);
+    scanf(" %[^\n]", estado1);  // jeitinho descoberto pelo chat gpt e a deepseek pra capturar o nome do estado com espaços
 
     printf("Digite o nome da cidade:\n");
     scanf(" %[^\n]", cidade1);
@@ -71,16 +72,26 @@ pibc2 = pib2 / populacao2;
     printf("Cartas cadastradas. Vejamos:\n");
 
     printf("Primeira carta:\n");
-    printf("Estado: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turísticos: %d\n", 
-           estado1, cidade1, populacao1, area1, pib1, turismo1);
+    printf("Estado: %s\nCidade: %s\nCódigo 1: %c01\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turísticos: %d\n", 
+           estado1, cidade1, cidade1[0], populacao1, area1, pib1, turismo1);
     printf("Densidade da primeira cidade: %.2f\n", dens1);       
-    printf("Pib per capita da primeira cidade: %.2f\n", dens1);
+    printf("Pib per capita da primeira cidade: %.2f\n", pibc1);
 
     printf("Segunda carta:\n");
-    printf("Estado: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turísticos: %d\n", 
-           estado2, cidade2, populacao2, area2, pib2, turismo2);       
+    printf("Estado: %s\nCidade: %s\nCódigo 2: %c02\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turísticos: %d\n", 
+           estado2, cidade2, cidade2[0], populacao2, area2, pib2, turismo2);
     printf("Densidade da segunda cidade: %.2f\n", dens2);
-    printf("Pib per capita da segunda cidade: %.2f\n", dens2);  
+    printf("Pib per capita da segunda cidade: %.2f\n", pibc2);  
+
+//calculo super poder (soma população, área, pib, pibc, 1/densi)
+
+    densInvertida1 = 1 / dens1;
+    densInvertida2 = 1 / dens2;
+    super1 = populacao1 + area1 + pib1 + densInvertida1 
+
+
+//comparação
+
 
     return 0;
 }
